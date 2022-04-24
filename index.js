4bd5713fc0fb9a16911f6311593ef6a40a9505d9
gilbert <eleazarbiboum@gmail.com>
2022-04-24 16:05:46 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here.
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent= "YOUR-NAME is the champion"
+
+
+
+
